f1479c31c605b5315b05b0b04fc979d0d5fef203
Jim Cooper <jim@cooperplanet.com>
2025-07-14 13:46:23 -0600

    Initial Commit

---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
+import { Contact } from '../contacts/contact.model';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
+
+  constructor(private contactsService: ContactsService) { }
+
+  ngOnInit() {
+    this.contactsService.getAllContacts().subscribe(contacts => this.contacts = contacts);
+  }
+
+  get favoriteContacts(): Contact[] {
+    return this.contacts
+      .filter(c => c.favoritesRanking && c.favoritesRanking > 0)
+      .sort(this.sortByFavoriteRanking);
+  }
+
+  sortByFavoriteRanking(a: Contact, b: Contact): number {
+    if (!a.favoritesRanking)
+      return -1;
+    if (!b.favoritesRanking)
+      return 1;
+    if (a.favoritesRanking < b.favoritesRanking)
+      return -1;
+    else if (a.favoritesRanking > b.favoritesRanking)
+      return 1;
+
+    return 0;
+  }
+}
